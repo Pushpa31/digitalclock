43762c88c893bead5bc09a1235616fea68de4a7c
Pushpapandey <pushpapandey799@gmail.com>
2019-11-12 16:37:17 +0545

    digital clock.

---
+++ watch.js
@@ -0,0 +1,31 @@
+function showTime()
+{
+	var date = new Date();
+	var h = date.getHours();
+	var m = date.getMinutes();
+	var s = date.getSeconds();
+	var session="AM";
+	if(h>12)
+	{
+		h=h-12;
+		session="PM";
+
+	}
+	if(m<10)
+	{
+		m = "0" + m;
+
+	}
+    if (s<10)
+{
+	s ="0" + s;
+}
+var time = h + ":" + m + ":" + s +""+ session ;
+document.getElementById("watch").innerHTML = time;
+setTimeout(showTime,1000)
+}
+showTime();
+
+
+
+
